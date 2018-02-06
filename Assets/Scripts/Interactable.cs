7ec3c9958ea31142590c7b33a576134fc7dfb0d5
Ákos Balázs <profakos@gmail.com>
2018-02-07 00:34:34 +0100

    Refactors reactions and message skipping

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour 
+{
+	public ReactionCollection reaction = new ReactionCollection ();
+
+	public ReactionCollection Interact()
+	{
+		return reaction;
+	}
+}
